42f4ddca07eac17c0be84d2d1ae8f40fe819e952
KyawZKK777 <moehtetkaungkin.kzkk@gmail.com>
2020-11-28 13:14:35 +0630

    znetapp

--- app.js
+++ app.js
@@ -1380,7 +1380,7 @@ const saveOrder = (arg, sender_psid) => {
     console.log('SAVED', success);
     let text = "Thank you. We have received your order."+ "\u000A";
     text += " We wil call you to confirm soon"+ "\u000A";
-    text += "Your booking reference number is:" + data.ref;
+    text += "Your order number is:" + data.ref;
     let response = {"text": text};
     callSend(sender_psid, response);
   }).catch((err)=>{
